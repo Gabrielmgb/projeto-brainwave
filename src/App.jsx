7d5a65c3c045752a3510670b44a69edc49772d37
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-05-26 23:30:23 -0300

    new assets

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-600'>
+      <p className='text-blue-500 text-center w-full mx-auto'>Olaaaa mundo</p>
+    </div>
+  )
+}
+
+export default App
